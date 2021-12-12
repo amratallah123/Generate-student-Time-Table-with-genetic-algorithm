34ecbb2b5873aa8bde30f3c220b2c144d7b2c72e
LENOVO <moedegy25@gmail.com>
2021-12-12 21:08:28 +0200

    Population.py

--- Population.py
+++ Population.py
@@ -6,4 +6,4 @@ class Population:
       self._scheduales=[]
       for i in range(0,size): self._scheduales.append(Schedule().initialize())
 
-  def get_scheduals(self): return self._scheduales
+  def get_scheduals(self): return self._schedualess
