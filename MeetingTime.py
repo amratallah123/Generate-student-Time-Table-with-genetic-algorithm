94244267579760a9998baa241b326688424160dc
MohamedMokhtar6 <96016335+MohamedMokhtar6@users.noreply.github.com>
2021-12-12 19:23:35 +0200

    Add files via upload

---
+++ MeetingTime.py
@@ -0,0 +1,6 @@
+class MeetingTime:
+    def init(self,id,time):
+        self._id=id
+        self._time=time
+    def get_id(self):return self._id
+    def get_time(self):return self._time
---
+++ Room.py
@@ -0,0 +1,7 @@
+class Room:
+    def intit(self,number,seatingCapacity):
+        self._number=number
+        self._seattingCapacity=seatingCapacity
+    def get_number(self):return self._number
+    def get_seatCapacity(self):return self._seattingCapacity
+
